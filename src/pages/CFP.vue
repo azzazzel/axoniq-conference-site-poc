5def2cd29ea3feae19f612036199ec761694d39c
Milen Dyankov <milendyankov@gmail.com>
2020-08-06 09:43:54 +0200

    fix typos

--- src/pages/CFP.vue
+++ src/pages/CFP.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page>
     <div class="absolute-center text-center">
-        <div class="text-h5">Thank you for your interest in speaking at Event-driven Microservices Confernce</div>
-        <div class="text-h2 q-ma-lg">Call for papers in now closed</div>
+        <div class="text-h5">Thank you for your interest in speaking at Event-driven Microservices Conference</div>
+        <div class="text-h2 q-ma-lg">Call for papers is now closed</div>
         <div class="text-h5">We would love to consider you as a speaker next year!</div>
     </div>
   </q-page>
